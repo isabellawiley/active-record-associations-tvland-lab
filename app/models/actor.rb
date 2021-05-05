cd688a5c2cbfdb4ddf323cfe9d407d351128170d
Isabella <wileyisabella@gmail.com>
2021-05-05 15:31:42 -0400

    commit tree e17c7e9c12af884885e2254c71b7ae94444dfad4

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    fname = self.first_name
+    lname = self.last_name
+    fname + " " + lname
+  end
+
+  def list_roles
+    # Show.all.map {|show| show.actor == self}
+    self.shows.map {|show| show.characters.find_by(:actor => self).name + " - " + show.name}
+  end
 end
